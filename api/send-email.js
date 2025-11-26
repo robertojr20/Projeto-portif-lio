9b171dceb0b2551ab7efd091ca0ae4921aaa09ae
robertojr20 <robertovdsj@fam.edu.br>
2025-11-26 17:22:06 -0300

    Arquivo send-email atualizado

--- api/send-email.js
+++ api/send-email.js
@@ -5,27 +5,31 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Método não permitido" });
   }
 
-  const { assunto,nome, email, mensagem } = req.body;
+  const { assunto, nome, email, mensagem } = req.body;
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // com gmail precisa ser true na porta 465
       auth: {
         user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
+        pass: process.env.MAIL_PASS, // PRECISA SER App Password
       },
     });
 
     await transporter.sendMail({
-      from: email,
+      from: process.env.MAIL_USER,   // nunca use o email do cliente
+      replyTo: email,
       to: process.env.TO_EMAIL,
       subject: assunto || "Nova mensagem do formulário",
-      text: `Assunto: ${assunto}\n Nome: ${nome}\nEmail: ${email}\nMensagem:\n${mensagem}`,
+      text: `Assunto: ${assunto}\nNome: ${nome}\nEmail: ${email}\nMensagem:\n${mensagem}`,
     });
 
     return res.status(200).json({ success: true });
+
   } catch (error) {
-    console.error(error);
+    console.error("Email error:", error);
     return res.status(500).json({ error: "Erro ao enviar email" });
   }
 }
